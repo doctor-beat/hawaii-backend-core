bf92c6cd0b18860cf4c7f42f05ecf848ed7cf5c9
ronald.verheul <r.verheul@triple-it.nl>
2017-02-20 15:15:29 +0100

    more logging

--- src/main/java/io/kahu/hawaii/rest/DefaultResponseManager.java
+++ src/main/java/io/kahu/hawaii/rest/DefaultResponseManager.java
@@ -100,10 +100,10 @@ public class DefaultResponseManager implements ResponseManager {
 
     private boolean mustLog(Throwable throwable) {
         if (throwable instanceof ValidationException) {
-            return ((ValidationException)throwable).containsRequestValidationError();
+            //return ((ValidationException)throwable).containsRequestValidationError();
         }
         if (throwable instanceof AuthorisationException) {
-            return false;
+            //return false;
         }
         return true;
     }
